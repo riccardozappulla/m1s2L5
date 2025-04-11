5d1114873a3f914fcc000cfff090b0fd57b0e8aa
riccardozappulla <riccardozappullacv@gmail.com>
2025-04-11 17:45:58 +0200

    Add files via upload

--- m1s2L5/es corretto.py
+++ m1s2L5/es corretto.py
@@ -1,7 +1,7 @@
 import datetime 
 import re
 
-def sMsPsA(comando):
+def sMsPsAsS(comando):
     comando = str.lower(comando)
     comando = re.sub(r'[^\w\s]', '', comando)
     comando=comando.replace("à","a")
@@ -9,24 +9,32 @@ def sMsPsA(comando):
     comando=comando.replace("ì","i")
     comando=comando.replace("ò","o")
     comando=comando.replace("ù","u")
+    comando=re.sub(r"\s+$", "", comando)
     return comando
 
-def assistente_virtuale(comando):    or "che giorno e"
-    if comando == "qual e la data di oggi" or "che giorno e":       
+def assistente_virtuale(comando):
+    if comando == "qual e la data di oggi":       
         oggi = datetime.datetime.today()         
         risposta = "La data di oggi è " + oggi.strftime("%d/%m/%Y")    
     elif comando == "che ore sono":        
         ora_attuale = datetime.datetime.now().time()          
         risposta = "L'ora attuale è " + ora_attuale.strftime("%H:%M")    
     elif comando == "come ti chiami":       
-        risposta = "Mi chiamo Assistente Virtuale"    
+        risposta = "Mi chiamo Assistente Virtuale" 
+    elif comando == "help":       
+        print("Qual è la data di oggi?")
+        print("Che ore sono?")
+        print("Come ti chiami?")
+        print("se non hai domande (Esci)")
+        risposta = " " 
     else:        
         risposta = "Non ho capito la tua domanda."  
     return risposta
     
-while True:    
-    comando_utente = input("Cosa vuoi sapere? ") 
-    comando_utente = sMsPsA(comando_utente)
+while True:
+    print("help o aiuto per maggiori informazioni")  
+    comando_utente = input("Cosa vuoi sapere?") 
+    comando_utente = sMsPsAsS(comando_utente)
     if comando_utente.lower() == "esci":        
         print("Arrivederci!")        
         break    
